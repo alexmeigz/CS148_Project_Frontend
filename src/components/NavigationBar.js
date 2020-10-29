9cb38f7072e45e817c85dce7f3dc81b84dd8d665
Joseph Ng <joseph_ng@ucsb.edu>
2020-10-29 17:45:54 +0000

    Initilized NavigationBar component

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,16 @@
+// NavigationBar.js
+// Engineer: Joseph Ng
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavigationBar() {
+    return (
+        // TODO
+        <div>
+      
+        </div>
+    );
+};
+
+export default NavigationBar;
