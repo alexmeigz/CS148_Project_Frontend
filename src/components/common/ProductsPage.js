9cec40320d3466f0138e32f73e1e91f24cd9d63d
Joseph Ng <joseph_ng@ucsb.edu>
2020-10-30 23:56:42 +0000

    Initialized

---
+++ src/components/common/ProductsPage.js
@@ -0,0 +1,15 @@
+// ProductsPage.js
+// Engineer: Joseph Ng
+
+import React from 'react';
+
+function DefaultHomePage () {
+    return (
+        // TODO
+        <div>
+            <h1>[ProductsPage]</h1>
+        </div>
+    );
+};
+
+export default ProductsPage;
