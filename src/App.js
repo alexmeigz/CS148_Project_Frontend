581613fb282cb36b4d0381593ec262a5774933ef
Joseph Ng <joseph_ng@ucsb.edu>
2020-10-31 00:05:37 +0000

    Initial Testing

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import NavigationBar from './components/common/NavigationBar';
 import ContactUsFooter from "./components/common/ContactUsFooter";
 
 import DefaultHomePage from './components/pages/DefaultHomePage';
-import MapsPage from '.components/pages/MapsPage';
-import VendorsPage from '.components/pages/VendorsPage';
-import RecipesPage from '.components/pages/RecipesPage';
+import MapsPage from './components/pages/MapsPage'
+import VendorsPage from './components/pages/VendorsPage';
+import RecipesPage from './components/pages/RecipesPage'
 import ProductsPage from './components/pages/ProductsPage';
 import LoginPage from "./components/pages/LoginPage";
 
-import LoggedInHomePage from './components/pages/LoggedInHomePage';
+//import LoggedInHomePage from './components/pages/LoggedInHomePage';
 
 function App() {
     return (
@@ -23,7 +23,7 @@ function App() {
             <NavigationBar />
             <Switch>
 		        <Route path="/" component={DefaultHomePage} exact />
-                <Route path="/maps" component={MapsPage} exact />
+                <Route path="/maps" component={MapsPage} />
                 <Route path="/vendors" component={VendorsPage} />
                 <Route path="/recipes" component={RecipesPage}  />
                 <Route path="/products" component={ProductsPage} />
