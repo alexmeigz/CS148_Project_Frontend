597bdf4a1f52a999afdb9c349df93d1e52a3d1e4
Joseph Ng <joseph_ng@ucsb.edu>
2020-10-31 23:48:51 +0000

    Initialized ErrorPage

---
+++ src/components/pages/ErrorPage.js
@@ -0,0 +1,15 @@
+// ErrorPage.js
+// Engineer: Joseph Ng
+
+import React from 'react';
+
+function ErrorPage () {
+    return (
+        // TODO
+        <div>
+            <h1>404: Page does not exist</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
