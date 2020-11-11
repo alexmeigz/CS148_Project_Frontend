e477e5840a47a746afaea2880d1347ec51b424ff
sriya112 <sriyaaluru@gmail.com>
2020-11-11 01:46:50 +0000

    map stuff need fixing

--- src/components/pages/MapsPage.js
+++ src/components/pages/MapsPage.js
@@ -2,12 +2,20 @@
 // Engineer: Joseph Ng
 
 import React from 'react';
+import BingMapsReact from "bingmaps-react";
+// import { ReactBingmaps } from 'react-bingmaps';
 
-function MapsPage () {
+
+
+function MapsPage() {
     return (
         // TODO
         <div>
             <h1>[MapsPage]</h1>
+            <BingMapsReact bingMapsKey="Al6xM6_6DfVwdCAvRULkiOWrW0SYTDfS13YApD5QUruQJ-fIi4IuobDFRNzHeFQB" />
+            {/* {console.log(<ReactBingmaps
+                bingmapKey="Al6xM6_6DfVwdCAvRULkiOWrW0SYTDfS13YApD5QUruQJ-fIi4IuobDFRNzHeFQB" >
+            </ReactBingmaps>)} */}
         </div>
     );
 };
