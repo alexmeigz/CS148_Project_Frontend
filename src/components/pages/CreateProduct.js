ee2a52b83011e32b7f6b8a660beefa46a09b2944
Joseph Ng <joseph_ng@ucsb.edu>
2020-11-27 02:06:14 +0000

    Just for testing

--- src/components/pages/CreateProduct.js
+++ src/components/pages/CreateProduct.js
@@ -29,8 +29,8 @@ function ProductForm(props) {
     const submitForm = (evt) => {  //send creds to backend, nested arrow function
       evt.preventDefault();
 	
-      //let server = "https://nutriflix-flask-backend.herokuapp.com/api"
-      let server = "http://localhost:8118/api"
+      let server = "https://nutriflix-flask-backend.herokuapp.com/api"
+      // let server = "http://localhost:8118/api"
     
       let url = `${server}/product/?`
       for(const param in state){
