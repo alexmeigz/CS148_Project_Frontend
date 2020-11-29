6d0d4f4f41ae501a8d04b238bb139d5db61cd79c
alexmeigz <alexmeigz@gmail.com>
2020-11-29 07:22:56 +0000

    ui and contact us page

--- src/components/pages/ContactUsPage.js
+++ src/components/pages/ContactUsPage.js
@@ -21,7 +21,32 @@ function ContactUsPage (props) {
             <NavigationBar isLoggedIn={props.isLoggedIn} onLoginChange={handleLoginChange} user={props.user} />
             {props.isLoggedIn ? <AccountInfoBar user={props.user} onUserChange={handleUserChange}/> : null}
 
-            <h1>[ContactUsPage]</h1>
+            <div className="container">
+                <h1> Contact Us </h1>
+
+                <p>
+                    This website is designed for educational purposes for UC Santa Barbara's CS 148: Computer Science Project class during the Fall of 2020 under Professor Chandra Krintz. 
+                </p>
+
+                <p>
+                    For inquiries, please email us at:
+                    <ul>
+                        <li> <a href="mailto:nutriflix.help@gmail.com"> nutriflix.help@gmail.com</a> </li>
+                    </ul>
+                </p>
+
+                <p>
+                    To contact the creators of the site, please email us at:
+                    <ul>
+                        <li> <a href="mailto:alexmei@cs.ucsb.edu"> alexmei@cs.ucsb.edu</a> </li>
+                        <li> <a href="mailto:joseph_ng@cs.ucsb.edu"> joseph_ng@cs.ucsb.edu</a> </li>
+                        <li> <a href="mailto:pranavacharya@cs.ucsb.edu"> pranavacharya@cs.ucsb.edu</a> </li>
+                        <li> <a href="mailto:sriyaaluru@cs.ucsb.edu"> sriyaaluru@cs.ucsb.edu</a> </li>
+                    </ul>
+                </p>
+            </div>
+            
+            
 
             <ContactUsFooter />
         </div>
