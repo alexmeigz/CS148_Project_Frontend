f191452c38bb77489ff8ed56b5fbea2237ca4397
sriya112 <sriyaaluru@gmail.com>
2020-12-03 06:26:11 +0000

    map search abr

--- src/components/pages/Maps1.js
+++ src/components/pages/Maps1.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-class Maps1 extends Component {
-    static defaultProps = {
+
+function Maps1(props) {
+
+    const defaultProps = {
         center: {
             lat: 59.95,
             lng: 30.33
@@ -12,14 +13,75 @@ class Maps1 extends Component {
         zoom: 11
     };
 
-    render() {
-        return (
-            // Important! Always set the container height explicitly
-            <div style={{ height: '100vh', width: '100%' }}>
+    const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+    const [state, updateState] = useState({
+        q: "",
+        longitude: 0,
+        latitude: 0
+        // showHideDemo: false
+    })
+
+    const [results, setResults] = useState({});
+
+    function handleChange(evt) { //updating form elements, nested function
+        const name = evt.target.name //defined in render
+        const value = evt.target.value //defined in render
+        //because we are using a single state object above to hold multiple properties, we must save off the current state first (b/c we are only updating part of the object).  To do this, we "spread" state via ...state and add it to the new copy of state that updateState is creating, followed by any updates we want:
+        updateState({
+            ...state,
+            [name]: value
+        })
+    }
+
+    const submitForm = (evt) => {  //send creds to backend, nested arrow function
+        // alert("button clicked")
+        evt.preventDefault();
+
+        let url = `https://developers.zomato.com/api/v2.1/locations?query=${state.q}`
+        let newUrl = url;
+        fetch(newUrl,
+            {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'user-key': 'b0a63821c480eabed3ce36ee8033df7d'
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data["user_has_addresses"]) {
+                    setResults(data)
+                    // state.showHideDemo = true;
+                    console.log(newUrl)
+                    alert(`${results["location_suggestions"][0]["title"]}`)
+                    console.log(data)
+
+                }
+                else {
+                    alert(`Error with parameters`)
+                }
+            })
+            .catch((error) => console.log("Search error: " + error))
+    }
+
+    // render() {
+    return (
+        <div>
+            <form onSubmit={submitForm}>
+                <div className="form_input">
+                    <label className="form_label" for="q"> Search: </label>
+                    <input className="form_field" type="text" value={state.q} name="q" onChange={handleChange} />
+                </div>
+                <center><input className="form_submit" type="submit" value="Submit" /></center>
+            </form>
+            < div style={{ height: '100vh', width: '100%' }
+            }>
                 <GoogleMapReact
                     bootstrapURLKeys={{ key: 'AIzaSyBZP0ZV1R148SNRp2uxco36NbK-675hKAE' }}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
+                    defaultCenter={defaultProps.center}
+                    defaultZoom={defaultProps.zoom}
                 >
                     <AnyReactComponent
                         lat={59.955413}
@@ -27,9 +89,10 @@ class Maps1 extends Component {
                         text="My Marker"
                     />
                 </GoogleMapReact>
-            </div>
-        );
-    }
+            </div >
+        </div>
+    );
+    // }
 }
 
 export default Maps1;
