4551f5aaf850ff24ba24d12b4add0f720ae5e5cd
sriya112 <sriyaaluru@gmail.com>
2020-12-07 01:23:54 +0000

    updated api key/id, fixed descriptions

--- src/components/pages/RecipeCall.js
+++ src/components/pages/RecipeCall.js
@@ -27,7 +27,7 @@ function RecipeCall(props) {
         // alert("button clicked")
         evt.preventDefault();
 
-        let url = `https://api.edamam.com/search?q=${state.q}&app_id=394088d7&app_key=cc0cb85ce098f755f855195231d1fe9f&from=0&to=60`
+        let url = `https://api.edamam.com/search?q=${state.q}&app_id=e1a55240&app_key=abe181a8a1ffbd0a236f1aac3381a621&from=0&to=60`
         let newUrl = url;
         if (state.health !== "") {
             newUrl += `&health=${state.health}`
@@ -75,7 +75,7 @@ function RecipeCall(props) {
                     <input className="form_field" type="text" value={state.q} name="q" onChange={handleChange} />
                 </div>
                 <div className="form_input">
-                    <label className="form_label" for="health"> Health: (ie paleo, low-sugar) </label>
+                    <label className="form_label" for="health"> Health: (ie vegan) </label>
                     <input className="form_field" type="text" value={state.health} name="health" onChange={handleChange} />
                 </div>
                 {/* <div className="form_input">
