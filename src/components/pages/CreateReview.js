471ddab29f3077c9eca44e3157604906948f73a9
alexmeigz <alexmeigz@gmail.com>
2020-12-08 07:20:04 +0000

    changes

--- src/components/pages/CreateReview.js
+++ src/components/pages/CreateReview.js
@@ -38,7 +38,7 @@ function ReviewForm(props) {
           server = "https://nutriflix-flask-backend.herokuapp.com/api"
       }
       if (process.env.NODE_ENV !== "development") {
-          server = "https://nutriflix-flask-backend.herokuapp.com//api"
+          server = "https://nutriflix-flask-backend.herokuapp.com/api"
       }
     
       let url = `${server}/post/?`
