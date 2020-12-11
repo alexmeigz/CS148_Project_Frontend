200c2bf60b3a211da7df9c6699ca5df9716b41c2
pranav01acharya <pranavacharya@umail.ucsb.edu>
2020-12-11 09:33:55 +0000

    added report button to blog view, error when making post reuest

---
+++ src/components/pages/AddReport.js
@@ -0,0 +1,84 @@
+// AddReport.js
+// Engineer: Pranav Acharya
+
+import React, {useState} from "react";
+
+function AddReport(props) {
+    let server = "http://localhost:8118/api"
+    if (process.env.REACT_APP_REMOTE === "1") { 
+        server = "https://nutriflix-flask-backend.herokuapp.com/api"
+    }
+    if (process.env.NODE_ENV !== "development") {
+        server = "https://nutriflix-flask-backend.herokuapp.com/api"
+    }
+
+    const [newInfo, setNewInfo] = useState({
+        ...props.reportData,
+        report_info: ""
+    })
+
+    function handleChange(evt) { 
+        const name = evt.target.name 
+        const value = evt.target.value 
+
+	  setNewInfo({
+        ...newInfo,
+        [name]: value
+      })
+    }
+
+    function submitReport(event) {
+        event.preventDefault();
+
+        let requiredParams = ["userReporter_id", "reportedUser_id"]
+
+        let url = `${server}/report/?`
+
+        requiredParams.forEach((param, index) => {
+            if (newInfo[param] === "") {
+                newInfo[param] = props.postData[param]
+            }
+            url += `&${param}=${newInfo[param]}`
+        });
+
+        console.log(url)
+
+        fetch(url, 
+            {
+                method: 'POST',
+                headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                }, 
+                body: props.newInfo[report_info],          
+            })
+            .then(response => response.json()) 
+                .then(data => {
+                if(data["message"] === "Report created successfully!"){
+                    alert("Report created successfully!")
+                    props.cancelReport()
+                }
+                else{
+                    alert(`Error creating report: ${data["message"]}`)
+                }
+            })
+            .catch((error) => console.log("Report creation error: " + error))
+
+    }
+
+    return (
+        <div className="update-form">
+            <h1> Add Report </h1>
+            <form onSubmit={submitReport}>
+                <div className="form_input">
+                    <label className="form_label" for="report_info"> Report: </label>         
+                    <textarea className="form_field" type="text" value={newInfo.report_info} name="report_info" onChange={handleChange} />
+                </div>
+                
+                <input className="form_submit" type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
+
+export default AddReport;
